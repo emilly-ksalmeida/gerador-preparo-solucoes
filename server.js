c3cee38379735b32c721a354760857e89899446e
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-03-16 16:26:33 -0300

    Adicionando trecho de código de outro projeto para testar conectar ao banco e get dos dados

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ import conectarAoBanco from "./dbconfig";
 const conexao = await conectarAoBanco(process.env.STRING_CONEXAO);
 const app = express();
 
+async function getTodosPosts() {
+    const db = conexao.db("imersao-instabytes"); //nome do banco de dados: imersao-instabytes
+    const colecao = db.collection("posts");
+    return colecao.find().toArray();
+
+}
+
+//Esta rota retorna todos os posts
+app.get("/posts", async (req, res) => {
+    const posts = await getTodosPosts();
+    res.status(200).json(posts);
+});
+
 app.listen(3000, () => {
     console.log("Servidor escutando...");
 });
