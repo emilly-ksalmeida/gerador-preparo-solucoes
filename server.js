105bc9bf9446f71b088e5a67b3dc1da6ecb7246d
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-03-30 16:15:48 -0300

    Adição da configuração do EJS no servidor

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import routes from "./src/routes/routes.js";
 
 const app = express();
-
+app.set("views", path.resolve(__dirname, "src", "views"));// Define a pasta onde estão os arquivos de visualização (templates)
+app.set("view engine", "ejs");// Define o motor de template a ser usado (neste caso, EJS)
 routes(app);
 
 app.listen(3000, () => {
