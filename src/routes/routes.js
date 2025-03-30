9932a0bdaa5b3663482576fc5c5d95f8777431c3
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-03-30 16:23:23 -0300

    Criando rota de teste para servir a página principal do site.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,6 +7,10 @@ const routes = (app) => {
     app.use(cors());//utilizando desta forma, o servidor irá aceitar requisições de API de qualquer origem.
     app.use(express.json()); //é necessário adicionar essa parte para que req possam ser passadas com body no formato de arquivo json
 
+    app.get("/home", (req, res) =>{
+        res.render(index);
+    });
+
     app.get("/posts", async (req, res) => {
         const posts = await getTodosPosts();
         res.status(200).json(posts);
