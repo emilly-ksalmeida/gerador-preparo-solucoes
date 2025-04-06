642388302deefafe8a0f0e8b98a70e69ce1cca9a
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-04-06 11:17:37 -0300

    Configurando pasta de arquivos estáticos para o servidor entregar a página home do site e o arquivo de style.

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const app = express();
 app.set("views", new URL("src/views", import.meta.url).pathname);// Define a pasta onde estão os arquivos de visualização (templates), forma compatível com ES6 modules.
 
 app.set("view engine", "ejs");// Define o motor de template a ser usado (neste caso, EJS)
+
+app.use(express.static(new URL("public", import.meta.url).pathname));// Configura o Express para servir arquivos estáticos (como CSS, imagens, JS) da pasta "public". O caminho absoluto é gerado com ES6 usando import.meta.url.
+
 routes(app);
 
 app.listen(3000, () => {
