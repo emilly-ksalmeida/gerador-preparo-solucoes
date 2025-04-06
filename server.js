4d8cadd7a54ebdba56ddbdc549339dfd56707944
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-04-06 10:06:47 -0300

    Modificando a forma de definir o caminho da pasta views para formato compatível com ES6 modules.

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 import express from "express";
 import routes from "./src/routes/routes.js";
-import path from "path";
 
 const app = express();
-app.set("views", path.resolve(__dirname, "src", "views"));// Define a pasta onde estão os arquivos de visualização (templates)
+
+app.set("views", new URL("src/views", import.meta.url).pathname);// Define a pasta onde estão os arquivos de visualização (templates), forma compatível com ES6 modules.
+
 app.set("view engine", "ejs");// Define o motor de template a ser usado (neste caso, EJS)
 routes(app);
 
