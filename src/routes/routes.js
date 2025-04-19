587bba6f6acd6827422c8c4e4491ffbd2d49811a
emilly-ksalmeida <emilly.eksa@gmail.com>
2025-04-19 16:25:08 -0300

    Corrigindo erro de acesso aos dados do formulário enviado no body da requisição. Foi adicionado o middleware que dá acesso aos campos do formulário enviado.

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -4,6 +4,7 @@ import { getTodosPosts } from "../models/models.js";
 import cors from "cors";
 
 const routes = (app) => {
+    app.use(express.urlencoded({ extended: true }));// Middleware do Express que faz o parsing de dados enviados via formulário (application/x-www-form-urlencoded), isso permite acessar os campos do formulário usando req.body
     app.use(cors());//utilizando desta forma, o servidor irá aceitar requisições de API de qualquer origem.
     app.use(express.json()); //é necessário adicionar essa parte para que req possam ser passadas com body no formato de arquivo json
 
@@ -24,8 +25,9 @@ const routes = (app) => {
     app.post("/posts", postarNovoPost);
 
     app.post("/rota-teste-form", (req, res)=> {
-        console.log(req.body);
-        res.send("Formulário recebido");
+        let respostaFormulario = req.body;
+        console.log(respostaFormulario);
+        res.send(respostaFormulario);
     })
     
     app.put("/upload/:idPesquisa", atualizarNovoPost);
